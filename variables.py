ae04fb66b3e69c358f698762cd94932b9bd8b112
ipco <ipcoo43@gmail.com>
2019-04-10 14:58:30 +0900

    4-10-1

---
+++ variables.py
@@ -0,0 +1,14 @@
+num=30     # integer
+num=3.24   # float
+print('3+4*5 =',3+4*5)
+print('(3+4)*5 =',(3+4)*5)
+print('3+4*5-4/2 =',3+4*5-4/2)
+print('2**10 =',2**10)
+print('5//2 =',5//2)
+print('5/2 =',5/2)
+print('type(4//2) =',type(4//2))
+print('type(4/2) =',type(4/2))
+print('21%2 =',21%2)
+print('20>10 =',20>10)
+print('20>10 and 5>=4',20>10 and 5>=4)
+print('not 5>10',not 5>10)
