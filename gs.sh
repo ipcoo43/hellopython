5a8a1a1ef61e612fd1a7f1ffbdaaeebc279861e8
ipco <ipcoo43@gmail.com>
2019-04-17 09:25:12 +0900

    lesson

--- gs.sh
+++ gs.sh
@@ -2,15 +2,16 @@
 
 # DATE = `date +%Y-%m-%d" "%H:%M`
 DATE = `date +%Y-%m-%d`
-MSG='$DATE lesson'
+MSG="$DATE lesson"
 
 if [ $# -gt 0 ];
 then
-	MSG='$1'
+	MSG="$1"
 fi
 
+git diff --numstat origin/master
+
 git add --all
+git commit -am "${MSG}"
 
-git commit -am '${MSG}'
-
-git push
+git push origin master
